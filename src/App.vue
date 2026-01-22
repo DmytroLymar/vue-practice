2251beca63d0601dd6ab5dd170eeac700af958ce
DmytroLymar <dmytrolymar901@gmail.com>
2026-01-22 14:35:09 +0200

    (8.2.3) reactivity fundamentals

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,52 @@
 <script>
+import { nextTick } from 'vue'
+
 export default {
   data() {
     return {
-      count: 0,
+      count: 1,
       msg: 'Hello world!',
       id: '22',
-      isButtonDisabled: true,
+      isButtonDisabled: false,
       seen: true,
+      obj: {
+        nested: { count: 0 },
+        arr: ['foo', 'bar'],
+      },
     }
+  },
+
+  methods: {
+    async increment() {
+      this.count++
+      await nextTick()
+    },
+    mutateDeeply() {
+      // these will work as expected.
+      this.obj.nested.count++
+      this.obj.arr.push('baz')
+    },
+  },
+
+  // `mounted` is a lifecycle hook which we will explain later
+  mounted() {
+    // `this` refers to the component instance.
+    console.log(this.count) // => 1
+
+    // data can be mutated as well
+    this.count = 7
   },
 }
 </script>
 
 <template>
   <div :id>
-    <button :disabled="isButtonDisabled" @click="count++">{{ count }}</button>
+    <button :disabled="isButtonDisabled" @click="increment">{{ count }}</button>
     <p>Message: {{ msg }}</p>
     <p v-if="seen">Now you see me</p>
+    <p>deep count: {{ obj.nested.count }}</p>
+    <p>deep arr: {{ obj.arr.join(' ') }}</p>
+    <button @click="mutateDeeply">Deep mutate</button>
   </div>
 </template>
 
