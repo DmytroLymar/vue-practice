237e6f0cfa0c115cada0d5f4632336aa32551131
DmytroLymar <dmytrolymar901@gmail.com>
2026-01-22 13:53:45 +0200

    (8.2.2) template syntax

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,21 @@ export default {
   data() {
     return {
       count: 0,
+      msg: 'Hello world!',
+      id: '22',
+      isButtonDisabled: true,
+      seen: true,
     }
   },
 }
 </script>
 
 <template>
-  <button @click="count++">{{ count }}</button>
+  <div :id>
+    <button :disabled="isButtonDisabled" @click="count++">{{ count }}</button>
+    <p>Message: {{ msg }}</p>
+    <p v-if="seen">Now you see me</p>
+  </div>
 </template>
 
 <style scoped></style>
