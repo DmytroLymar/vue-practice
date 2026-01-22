d716a182007c801f5e7d919c4e386722e092643e
Dmytro Lymar <dmytrolymar901@gmail.com>
2026-01-22 18:37:22 +0200

    (8.2.9) form input bindings

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,74 @@
 <script setup>
 import { ref } from 'vue'
 
-const name = ref('Vue.js')
+const text = ref('Vue.js')
+const message = ref('')
+const messageMulti = ref('')
+const checked = ref(false)
+const checkedNames = ref([])
+const picked = ref('One')
+const selected = ref('A')
 
-function greet(event) {
-  alert(`Hello ${name.value}!`)
-  // `event` is the native DOM event
-  if (event) {
-    alert(event.target.tagName)
-  }
-}
+const options = ref([
+  { text: 'One', value: 'A' },
+  { text: 'Two', value: 'B' },
+  { text: 'Three', value: 'C' },
+])
 
-function say(message) {
-  alert(message)
-}
-
-function warn(message, event) {
-  // now we have access to the native event
-  if (event) {
-    event.preventDefault()
-  }
-  alert(message)
-}
+const toggle = ref('yes')
+const age = ref(10)
 </script>
 
 <template>
-  <button @click="greet">Greet</button>
-  <button @click="say('hello')">Say hello</button>
-  <button @click="say('bye')">Say bye</button>
-  <button @click="warn('Form cannot be submitted yet.', $event)">Submit</button>
-  <button @click="(event) => warn('Form cannot be submitted yet.', event)">Submit</button>
+  <input v-model="text" />
+  <p>Message is: {{ message }}</p>
+  <input v-model="message" placeholder="edit me" />
+  <div></div>
+  <span>Multiline message is:</span>
+  <p style="white-space: pre-line">{{ messageMulti }}</p>
+  <textarea v-model="messageMulti" placeholder="add multiple lines"></textarea>
+  <div></div>
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
+
+  <div>Checked names: {{ checkedNames }}</div>
+
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+  <label for="jack">Jack</label>
+
+  <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+  <label for="john">John</label>
+
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+  <label for="mike">Mike</label>
+
+  <div>Picked: {{ picked }}</div>
+
+  <input type="radio" id="one" value="One" v-model="picked" />
+  <label for="one">One</label>
+
+  <input type="radio" id="two" value="Two" v-model="picked" />
+  <label for="two">Two</label>
+
+  <div>Selected: {{ selected }}</div>
+
+  <select v-model="selected">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+
+  <div>Selected: {{ selected }}</div>
+
+  <select v-model="selected">
+    <option v-for="option in options" :value="option.value" :key="option.text">
+      {{ option.text }}
+    </option>
+  </select>
+
+  <input type="checkbox" v-model="toggle" true-value="yes" false-value="no" />
+  <input v-model.lazy="message" />
+  <input v-model.number="age" />
 </template>
 
 <style scoped></style>
