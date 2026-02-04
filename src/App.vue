d5b9ccdd08dc2f6889d6275bc704580d7e919a9b
Dmytro Lymar <dmytrolymar901@gmail.com>
2026-02-04 17:00:57 +0200

    Dockerfile added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script setup>
-import Dashboard from './components/dashboard/Dashboard.vue'
+import { ref } from 'vue'
+import ComponentA from './components/ComponentA.vue'
+import ComponentB from './components/ComponentB.vue'
+import TeleportedModal from './components/TeleportedModal.vue'
+
+const current = ref('ComponentA')
+
+const components = {
+  ComponentA,
+  ComponentB,
+}
 </script>
 
 <template>
-  <Suspense>
-    <Dashboard />
+  <label><input type="radio" v-model="current" value="ComponentA" /> A</label>
+  <label><input type="radio" v-model="current" value="ComponentB" /> B</label>
 
-    <template #fallback>
-      <p>Loading dashboard...</p>
-    </template>
-  </Suspense>
+  <KeepAlive>
+    <component :is="components[current]"></component>
+  </KeepAlive>
+  <TeleportedModal />
 </template>
